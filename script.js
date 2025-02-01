2ecea311781fb47bb9f37026126ba275c20d0c90
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-01 15:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-return key in sampleObject;
+if(sampleObject.hasOwnProperty(key)){
+	return true;
+}
+	return false;
 }
  
 // Do not change the code below
