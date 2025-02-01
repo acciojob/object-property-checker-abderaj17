afddf6bc2fbc2f94699e855d09b60a01f04cd0db
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-01 15:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-return sampleObject.hasOwnProperty(key);
+return key in sampleObject;
 }
-
+ 
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key)); 
